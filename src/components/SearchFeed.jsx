82b644fd2f10b9c2485c7dda5ebc91396c689a67
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-10-31 08:35:31 -0700

    Update SearchFeed.jsx

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -20,7 +20,7 @@ const SearchFeed = () => {
   return (
     <Box p={2} sx={{ overflowY: 'auto', height: '90vh',flex: 2 }}>
       <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }} >
-       Search Results for: <span style={{ color: '#f31503' }}>{searchTerm}Videos</span>
+       Search Results for: <span style={{ color: '#f31503' }}>{searchTerm}</span>  Videos
       </Typography>
       <Videos videos={videos} />
     </Box>
